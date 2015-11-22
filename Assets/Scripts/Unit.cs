4f74feb2042e7b9fd769a0cab9f3f6167a9175a7
Nepfion <nepfion96@gmail.com>
2015-11-22 22:50:31 +0300

    I have added following scripts:\nAlies, Enemy, Game, Unit and realised Move-function and setTarget-function

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : GameObject {
+
+	private Unit target;
+	private Sprite unitSprite;
+	private int HealthPoints;
+	private void Strike ();
+
+	public Unit();
+	public void Init();
+	public void addStuff(string item);
+	public void Hit (int k);
+	public void SetTarget (Unit newTarget);
+	public void Move (Vector2 vector);
+	public const float unitSpeed;
+	
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+		Strike ();
+	}
+
+	void Move(Vector2 vector)
+	{
+		transform.Translate (vector * Time.deltaTime * unitSpeed, Space.World);
+	}
+
+	void SetTarget (Unit newTarget)
+	{
+		target = newTarget;
+	}
+}
