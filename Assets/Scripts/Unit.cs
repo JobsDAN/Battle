926cee434acd1c79e1d2a3811264540f08d77f85
Nepfion <nepfion96@gmail.com>
2015-11-22 23:20:46 +0300

    code refactoring and Hit-function

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -9,14 +9,34 @@ public class Unit {
 	private GameObject GOUnit;
 	private void Strike ();
 
-	public Unit(GameObject newUnit);
-	public void Init();
-	public void addStuff(string item);
-	public void Hit (int k);
-	public void SetTarget (Unit newTarget);
-	public void Move (Vector2 vector);
-
 	public const float unitSpeed;
+	public Unit (GameObject newUnit)
+	{
+		GOUnit = newUnit;
+		HealthPoints = 100;
+	}
+
+	public void addStuff(string item)
+	{
+
+	}
+
+	public void Hit (int k)
+	{
+		target.HealthPoints -= k;
+	}
+
+	public void SetTarget (Unit newTarget)
+	{
+		target = newTarget;
+	}
+
+	public void Move(Vector2 vector)
+	{
+		GOUnit.transform.Translate (vector * Time.deltaTime * unitSpeed, Space.World);
+	}
+
+
 	
 	void Start () {
 	
@@ -25,20 +45,5 @@ public class Unit {
 
 	void Update () {
 		Strike ();
-	}
-
-	Unit (GameObject newUnit)
-	{
-		GOUnit = newUnit;
-	}
-
-	void Move(Vector2 vector)
-	{
-		GOUnit.transform.Translate (vector * Time.deltaTime * unitSpeed, Space.World);
-	}
-
-	void SetTarget (Unit newTarget)
-	{
-		target = newTarget;
 	}
 }
