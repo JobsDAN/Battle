487ecee200420de2570038ed3f194787ae5c7540
Nepfion <nepfion96@gmail.com>
2015-11-22 23:05:40 +0300

    I have rebuilt NASLEDOVANIE from GameObject, now it NASLEDUETSYA from nothing

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,19 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Unit : GameObject {
+public class Unit {
 
 	private Unit target;
 	private Sprite unitSprite;
 	private int HealthPoints;
+	private GameObject GOUnit;
 	private void Strike ();
 
-	public Unit();
+	public Unit(GameObject newUnit);
 	public void Init();
 	public void addStuff(string item);
 	public void Hit (int k);
 	public void SetTarget (Unit newTarget);
 	public void Move (Vector2 vector);
+
 	public const float unitSpeed;
 	
 	void Start () {
@@ -25,9 +27,14 @@ public class Unit : GameObject {
 		Strike ();
 	}
 
+	Unit (GameObject newUnit)
+	{
+		GOUnit = newUnit;
+	}
+
 	void Move(Vector2 vector)
 	{
-		transform.Translate (vector * Time.deltaTime * unitSpeed, Space.World);
+		GOUnit.transform.Translate (vector * Time.deltaTime * unitSpeed, Space.World);
 	}
 
 	void SetTarget (Unit newTarget)
